4b08533abb035af15451071f6683f83e66dbac1b
rimus <rimus.cx@gmail.com>
2019-02-13 11:09:16 +0300

    Refactoring DL counter

--- src/main/java/by/rimus/dev/Runner.java
+++ src/main/java/by/rimus/dev/Runner.java
@@ -23,11 +23,10 @@ public class Runner {
         int rb = Parameters.getInstance().getRb();
         int rt = Team.getSumWithCoeffs(factTopSix) * rb / Team.getSumWithCoeffs(baseTopSix);
         int rg = team.getRating() * rt / rb;
-        int dl = rg * Parameters.getInstance().getFails() / Parameters.getInstance().getQuestions() / RATIO;
+        double dl = rg * Parameters.getInstance().getFails() / Parameters.getInstance().getQuestions() / RATIO;
         System.out.println("RG: " + rg);
-        System.out.println("DL: " + dl);
+        System.out.printf("DL: %.0f" + dl);
     }
-
 
     private static void parseCli(String[] args) {
         JCommander jCommander = new JCommander(Parameters.getInstance());
